33534983cb980caefe9ddcb1dd71860f4169f6db
Parth Agrawal <ag.parth1024@gmail.com>
2020-07-22 21:03:22 +0530

    Heuristics resolved

--- src/PathFinder/Heuristics.js
+++ src/PathFinder/Heuristics.js
@@ -1,13 +1,13 @@
 
 export function manhattan(dx,dy){
-  return dx+dy;
+  return Math.abs(dx)+ Math.abs(dy);
 }
 export function euclidean(dx,dy){
   return Math.sqrt(dx*dx + dy*dy)
 } 
 
 export function chebyshev(dx,dy){
-  return Math.max(dx,dy)
+  return Math.max(Math.abs(dx),Math.abs(dy))
 }
 
 
