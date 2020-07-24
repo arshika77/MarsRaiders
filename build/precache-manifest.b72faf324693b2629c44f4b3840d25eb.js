2a6f19c86e3b50b305f02407af52594b2b5c51e8
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2020-07-24 14:35:44 +0530

    adjusted padding

---
+++ build/precache-manifest.b72faf324693b2629c44f4b3840d25eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243c597570dbcb5374175e4d736be597",
+    "url": "/MarsRaiders/index.html"
+  },
+  {
+    "revision": "245f58393a77d78cbc3c",
+    "url": "/MarsRaiders/static/css/main.d2499277.chunk.css"
+  },
+  {
+    "revision": "5a37751d09b3ee7680c9",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245f58393a77d78cbc3c",
+    "url": "/MarsRaiders/static/js/main.7a9025fd.chunk.js"
+  },
+  {
+    "revision": "a4b5a03c4acb740b7509",
+    "url": "/MarsRaiders/static/js/runtime-main.a186da62.js"
+  }
+]);
