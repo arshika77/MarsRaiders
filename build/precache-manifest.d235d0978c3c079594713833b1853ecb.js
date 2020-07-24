ea11ad8a67823be31e2e7774092a093edbf8dab0
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2020-07-24 14:27:10 +0530

    Update URL in Reset Grid

---
+++ build/precache-manifest.d235d0978c3c079594713833b1853ecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee8083f21aec0860a371ff765de5065",
+    "url": "/MarsRaiders/index.html"
+  },
+  {
+    "revision": "1673fd4634d300d66adf",
+    "url": "/MarsRaiders/static/css/main.88832de6.chunk.css"
+  },
+  {
+    "revision": "5a37751d09b3ee7680c9",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1673fd4634d300d66adf",
+    "url": "/MarsRaiders/static/js/main.7a9025fd.chunk.js"
+  },
+  {
+    "revision": "a4b5a03c4acb740b7509",
+    "url": "/MarsRaiders/static/js/runtime-main.a186da62.js"
+  }
+]);
