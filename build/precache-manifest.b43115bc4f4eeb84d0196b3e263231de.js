a7314fb3e9f4a4da34a4cf54f8675132db3496f4
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2020-07-24 17:32:11 +0530

    Website Completed!

---
+++ build/precache-manifest.b43115bc4f4eeb84d0196b3e263231de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4441164f24bafe072d281ea0e94af9ac",
+    "url": "/MarsRaiders/index.html"
+  },
+  {
+    "revision": "24cfe72a984ff6718169",
+    "url": "/MarsRaiders/static/css/main.51ce4390.chunk.css"
+  },
+  {
+    "revision": "5a37751d09b3ee7680c9",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cfe72a984ff6718169",
+    "url": "/MarsRaiders/static/js/main.a9b73211.chunk.js"
+  },
+  {
+    "revision": "a4b5a03c4acb740b7509",
+    "url": "/MarsRaiders/static/js/runtime-main.a186da62.js"
+  }
+]);
