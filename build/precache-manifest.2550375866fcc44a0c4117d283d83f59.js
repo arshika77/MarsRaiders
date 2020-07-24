7552435ec2ebf44a4add7f20ecf186c3ce68aec7
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2020-07-24 14:54:08 +0530

    updated icon

---
+++ build/precache-manifest.2550375866fcc44a0c4117d283d83f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8993c39982fb7fb5014319bd891e28",
+    "url": "/MarsRaiders/index.html"
+  },
+  {
+    "revision": "0a32ab83de82191a2991",
+    "url": "/MarsRaiders/static/css/main.ede0d3f3.chunk.css"
+  },
+  {
+    "revision": "5a37751d09b3ee7680c9",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a32ab83de82191a2991",
+    "url": "/MarsRaiders/static/js/main.7a9025fd.chunk.js"
+  },
+  {
+    "revision": "a4b5a03c4acb740b7509",
+    "url": "/MarsRaiders/static/js/runtime-main.a186da62.js"
+  }
+]);
