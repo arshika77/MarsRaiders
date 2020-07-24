8d352a95964ccd6b0731d9896d7f719fec8e6e2d
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2020-07-24 13:44:34 +0530

    Updates

---
+++ precache-manifest.19b777cfc3b608497bfa31a77a5a1019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac8c3625e77ff64a36b3d7b5da22f09",
+    "url": "/MarsRaiders/index.html"
+  },
+  {
+    "revision": "b731716f0f77d0ebbff7",
+    "url": "/MarsRaiders/static/css/main.88832de6.chunk.css"
+  },
+  {
+    "revision": "5a37751d09b3ee7680c9",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/MarsRaiders/static/js/2.27fa5c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b731716f0f77d0ebbff7",
+    "url": "/MarsRaiders/static/js/main.4fd803cb.chunk.js"
+  },
+  {
+    "revision": "a4b5a03c4acb740b7509",
+    "url": "/MarsRaiders/static/js/runtime-main.a186da62.js"
+  }
+]);
